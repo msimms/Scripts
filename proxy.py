1f5b0dde1b50fbec9f7fd30516676fc8bcb28954
Mike Simms <msimms01@gmail.com>
2018-08-02 21:03:33 -0400

    Basic proxy functionality now working.

--- proxy.py
+++ proxy.py
@@ -21,73 +21,142 @@
 # SOFTWARE.
 
 import argparse
+import signal
 import socket
 import sys
 import threading
 import time
 
 g_server_thread = None
+g_client_threads = []
 
 def signal_handler(signal, frame):
     global g_server_thread
+    global g_client_threads
 
+    print "Exiting..."
+    print "Killing the server thread..."
     if g_server_thread:
         g_server_thread.terminate()
+        g_server_thread.join()
+    print "Killing client read threads..."
+    for client_thread in g_client_threads:
+        client_thread.terminate()
+        client_thread.join()
+    print "Done"
 
 class ClientReadThread(threading.Thread):
     """An instance of this class is created for each client connection."""
 
     def __init__(self, conn, addr, destip, destport):
         threading.Thread.__init__(self)
-        self.conn = conn
-        self.addr = addr
-        self.destip = destip
-        self.destport = destport
-
-    def run(self):
-        """Main run loop."""
-        data = self.conn.recv(10)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.destip, self.destport))
-        s.send(data)
-        s.close()
-        self.conn.close()
-
-class ServerProxy(threading.Thread):
-    """This class listens for connections from clients and spawns a thread to manage each connection."""
-
-    def __init__(self, sourceip, sourceport, destip, destport):
-        threading.Thread.__init__(self)
         self.stopped = threading.Event()
-        self.sourceip = sourceip
-        self.sourceport = sourceport
+        self.serversocket = None
+        self.clientsocket = conn
+        self.addr = addr
         self.destip = destip
         self.destport = destport
 
     def terminate(self):
         """Destructor"""
-        print "Terminating"
+        global g_client_threads
+
+        print "Terminating a client read thread."
+        if self in g_client_threads:
+            g_client_threads.remove(self)
         self.stopped.set()
+        if self.serversocket is not None:
+            self.serversocket.close()
+        if self.clientsocket is not None:
+            self.clientsocket.close()
+        print "Done terminating a client read thread."
+
+    def read_client_data(self):
+        """Read the next byte and send it to the ultimate destination."""
+        try:
+            data = self.clientsocket.recv(65536, socket.MSG_PEEK)
+            data_len = len(data)
+            if data_len > 0:
+                data = self.clientsocket.recv(data_len)
+                self.serversocket.send(data)
+        except:
+            self.stopped.set()
+
+    def read_server_data(self):
+        """Read the response, if nay, and send it back to the origin."""
+        try:
+            data = self.serversocket.recv(65536, socket.MSG_PEEK)
+            data_len = len(data)
+            if data_len > 0:
+                data = self.serversocket.recv(data_len)
+                self.clientsocket.send(data)
+        except:
+            self.stopped.set()
 
     def run(self):
         """Main run loop."""
-        print "Starting a listen thread on " + self.host + ":" + str(self.port)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.sourceip, self.sourceport))
-        while not self.stopped.wait(10):
-            s.listen(1)
-            conn, addr = s.accept()
-            client_thread = ClientReadThread(conn, addr, self.destip, self.destport)
-            client_thread.start()
-        s.close()
+        global g_client_threads
+
+        print "Starting a client read thread."
+
+        # Connect to the ultimate destination.
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.connect((self.destip, self.destport))
+
+        while not self.stopped.is_set():
+            self.read_client_data()
+            self.read_server_data()
+
+        if self.serversocket is not None:
+            self.serversocket.close()
+        if self.clientsocket is not None:
+            self.clientsocket.close()
+
+        print "Closed a client read socket."
+
+class ServerProxy(threading.Thread):
+    """This class listens for connections from clients and spawns a thread to manage each connection."""
+
+    def __init__(self, bindip, bindport, destip, destport):
+        threading.Thread.__init__(self)
+        self.stopped = threading.Event()
+        self.bindip = bindip
+        self.bindport = bindport
+        self.destip = destip
+        self.destport = destport
+        self.proxysocket = None
+
+    def terminate(self):
+        """Destructor"""
+        print "Terminating the server proxy."
+        self.stopped.set()
+        if self.proxysocket is not None:
+            print "Closing the server socket."
+            self.proxysocket.close()
+        print "Done terminating the server proxy."
+
+    def run(self):
+        """Main run loop."""
+        print "Starting a listen thread on " + self.bindip + ":" + str(self.bindport) + "."
+        self.proxysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.proxysocket.bind((self.bindip, self.bindport))
+        self.proxysocket.listen(1)
+        while not self.stopped.is_set():
+            conn, addr = self.proxysocket.accept()
+            if conn is not None:
+                client_thread = ClientReadThread(conn, addr, self.destip, self.destport)
+                g_client_threads.append(client_thread) # Register the new thread for signals
+                client_thread.start()
+        self.proxysocket.close()
+        print "Closed the server listen thread."
 
 def main():
     global g_server_thread
 
     # Parse command line options.
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sourceip", default="127.0.0.1", help="Host name on which to bind", required=False)
-    parser.add_argument("--sourceport", type=int, default=8080, help="Port on which to bind", required=False)
+    parser.add_argument("--bindip", default="127.0.0.1", help="Host name on which to bind", required=False)
+    parser.add_argument("--bindport", type=int, default=8080, help="Port on which to bind", required=False)
     parser.add_argument("--destip", default="127.0.0.1", help="Host name to which data is sent", required=False)
     parser.add_argument("--destport", type=int, default=8080, help="Port to which data is sent", required=False)
 
@@ -97,7 +166,9 @@ def main():
         parser.error(e)
         sys.exit(1)
 
-    g_server_thread = ServerProxy(args.sourceip, args.sourceport, args.destip, args.destport)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    g_server_thread = ServerProxy(args.bindip, args.bindport, args.destip, args.destport)
     g_server_thread.start()
 
     # Wait for it to finish. We do it like this so that the main thread isn't blocked and can execute the signal handler.
