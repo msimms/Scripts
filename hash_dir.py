a0b42a2947f5560c1f2954a476eec8cfa91431bc
Mike Simms <msimms01@gmail.com>
2018-09-06 15:34:13 -0400

    Added license and missing import.

--- hash_dir.py
+++ hash_dir.py
@@ -1,9 +1,32 @@
+# MIT License
+# 
+# Copyright (c) 2018 Mike Simms
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import argparse
 import glob
 import hashlib
 import os
 import shutil
 import subprocess
+import sys
 
 def normjoin(*args):
 	return os.path.normpath(os.path.join(*args))
@@ -26,7 +49,7 @@ def hash_dir(dir, rename_file, git_move_file, extension):
 			hash_str = hash_file(file_to_hash)
 			print file_to_hash + " hashes to " + hash_str
 			
-			path, name = os.path.split(file_to_hash)
+			path, _ = os.path.split(file_to_hash)
 			new_file = os.path.join(path, hash_str)
 			if extension is not None and len(extension) > 0:
 				new_file = new_file + "." + extension
